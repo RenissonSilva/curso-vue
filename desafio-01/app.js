192abc33a9faa08e041d89bbcaa8129a0b7f0aac
renissonsilva <renissonx@gmail.com>
2020-12-15 15:31:18 -0300

    Desafio 01

---
+++ desafio-01/app.js
@@ -0,0 +1,8 @@
+new Vue ({
+    el: '#desafio',
+    data: {
+        nome: 'Renisson Silva',
+        idade: '25',
+        logo: 'https://3lhowb48prep40031529g5yj-wpengine.netdna-ssl.com/wp-content/uploads/2019/10/logo-vuejs-min.png',
+    }
+})
